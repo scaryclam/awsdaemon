993b2f0113938d914c2602f3a00d99059ebca5b0
Becky Lewis <git@scaryclam.co.uk>
2014-08-26 14:20:04 +0000

    Added json parsing

--- src/co/uk/tangentlabs/aws/triggers/EC2Trigger.java
+++ src/co/uk/tangentlabs/aws/triggers/EC2Trigger.java
@@ -1,13 +1,25 @@
 package co.uk.tangentlabs.aws.triggers;
 
+import org.json.JSONObject;
+
+import com.amazonaws.services.ec2.model.Instance;
+
+import co.uk.tangentlabs.aws.EC2Inspector;
+
 public class EC2Trigger implements ITrigger {
     private static String TYPE = "EC2";
     private String name;
     private String instanceID;
     
-    public EC2Trigger(String name, String instanceID) {
+    public EC2Trigger(String name, JSONObject jsonConfig) {
         this.name = name;
-        this.instanceID = instanceID;
+        this.instanceID = jsonConfig.getString("instanceID");
+        System.out.println("EC2Trigger created");
+    }
+    
+    public EC2Trigger(String name, JSONObject jsonConfig, Instance instance) {
+        this.name = name;
+        this.instanceID = jsonConfig.getString("instanceID");
         System.out.println("EC2Trigger created");
     }
     
@@ -26,6 +38,13 @@ public class EC2Trigger implements ITrigger {
 
     @Override
     public void checkTrigger() {        
+    }
+    
+    public static void main(String[] args) {
+        String testJsonStr = "{'instanceID': '12345'}";
+        JSONObject testJson = new JSONObject(testJsonStr);
+        EC2Trigger trigger = new EC2Trigger("trigger 1", testJson);
+        trigger.getName();
     }
 }
 
